27e965d9ea38ae6fc30acf73543150a24c153a69
Ain Tohvri <at@interactive-pioneers.de>
2017-05-17 15:34:24 +0200

    Resolve #3 Scaffold generator with 2 options

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,71 +4,128 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-pioneerscss:app', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ipScss: true});
+  describe('with IP SCSS', () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ipScss: true});
+    });
+
+    it('creates entry point', () => {
+      assert.file([
+        'styles.scss'
+      ]);
+    });
+
+    it('creates base', () => {
+      assert.file([
+        'base/_media.scss',
+        'base/_page.scss'
+      ]);
+    });
+
+    it('creates components', () => {
+      assert.file([
+        'components/.gitkeep'
+      ]);
+    });
+
+    it('creates generic', () => {
+      assert.file([
+        'generic/_box-sizing.scss'
+      ]);
+    });
+
+    it('creates objects', () => {
+      assert.file([
+        'objects/_layout.scss',
+        'objects/_picturefill-bg.scss',
+        'objects/_responsive-video.scss',
+        'objects/_square.scss'
+      ]);
+    });
+
+    it('creates settings', () => {
+      assert.file([
+        'settings/_breakpoints.scss',
+        'settings/_colours.scss',
+        'settings/_easing.scss',
+        'settings/_fonts.scss',
+        'settings/_layout.scss'
+      ]);
+    });
+
+    it('creates tools', () => {
+      assert.file([
+        'tools/_font-size.scss',
+        'tools/_math.scss',
+        'tools/_media-queries.scss',
+        'tools/_shapes.scss',
+        'tools/_widths.scss'
+      ]);
+    });
+
+    it('creates trumps', () => {
+      assert.file([
+        'trumps/_clearfix.scss',
+        'trumps/_visibility.scss',
+        'trumps/_widths-responsive.scss',
+        'trumps/_widths.scss'
+      ]);
+    });
   });
 
-  it('creates entry point', () => {
-    assert.file([
-      'styles.scss'
-    ]);
-  });
+  describe('without IP SCSS', () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ipScss: false});
+    });
 
-  it('creates base', () => {
-    assert.file([
-      'base/_media.scss',
-      'base/_page.scss'
-    ]);
-  });
+    it('creates entry point', () => {
+      assert.file([
+        'styles.scss'
+      ]);
+    });
 
-  it('creates components', () => {
-    assert.file([
-      'components/.gitkeep'
-    ]);
-  });
+    it('creates base', () => {
+      assert.file([
+        'base/.gitkeep'
+      ]);
+    });
 
-  it('creates generic', () => {
-    assert.file([
-      'generic/_box-sizing.scss'
-    ]);
-  });
+    it('creates components', () => {
+      assert.file([
+        'components/.gitkeep'
+      ]);
+    });
 
-  it('creates objects', () => {
-    assert.file([
-      'objects/_layout.scss',
-      'objects/_picturefill-bg.scss',
-      'objects/_responsive-video.scss',
-      'objects/_square.scss'
-    ]);
-  });
+    it('creates generic', () => {
+      assert.file([
+        'generic/.gitkeep'
+      ]);
+    });
 
-  it('creates settings', () => {
-    assert.file([
-      'settings/_breakpoints.scss',
-      'settings/_colours.scss',
-      'settings/_easing.scss',
-      'settings/_fonts.scss',
-      'settings/_layout.scss'
-    ]);
-  });
+    it('creates objects', () => {
+      assert.file([
+        'objects/.gitkeep'
+      ]);
+    });
 
-  it('creates tools', () => {
-    assert.file([
-      'tools/_font-size.scss',
-      'tools/_math.scss',
-      'tools/_media-queries.scss',
-      'tools/_shapes.scss',
-      'tools/_widths.scss'
-    ]);
-  });
+    it('creates settings', () => {
+      assert.file([
+        'settings/.gitkeep'
+      ]);
+    });
 
-  it('creates trumps', () => {
-    assert.file([
-      'trumps/_clearfix.scss',
-      'trumps/_visibility.scss',
-      'trumps/_widths-responsive.scss',
-      'trumps/_widths.scss'
-    ]);
+    it('creates tools', () => {
+      assert.file([
+        'tools/.gitkeep'
+      ]);
+    });
+
+    it('creates trumps', () => {
+      assert.file([
+        'trumps/.gitkeep'
+      ]);
+    });
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -30,133 +30,142 @@ module.exports = class extends Generator {
     );
 
     this.fs.copy(
-      this.templatePath('src/base/_media.scss'),
-      this.destinationPath('base/_media.scss')
-    );
-
-    this.fs.copy(
-      this.templatePath('src/base/_page.scss'),
-      this.destinationPath('base/_page.scss')
-    );
-
-    this.fs.copy(
       this.templatePath('src/components/.gitkeep'),
       this.destinationPath('components/.gitkeep')
     );
 
-    this.fs.copy(
-      this.templatePath('src/generic/_box-sizing.scss'),
-      this.destinationPath('generic/_box-sizing.scss')
-    );
+    if (this.props.ipScss) {
+      this.fs.copy(
+        this.templatePath('src/base/_media.scss'),
+        this.destinationPath('base/_media.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/generic/_font-smoothing.scss'),
-      this.destinationPath('generic/_font-smoothing.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/base/_page.scss'),
+        this.destinationPath('base/_page.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/generic/_helper.scss'),
-      this.destinationPath('generic/_helper.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/generic/_box-sizing.scss'),
+        this.destinationPath('generic/_box-sizing.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/generic/_layout.scss'),
-      this.destinationPath('generic/_layout.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/generic/_font-smoothing.scss'),
+        this.destinationPath('generic/_font-smoothing.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/generic/_smooth-scrolling.scss'),
-      this.destinationPath('generic/_smooth-scrolling.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/generic/_helper.scss'),
+        this.destinationPath('generic/_helper.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/objects/_layout.scss'),
-      this.destinationPath('objects/_layout.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/generic/_layout.scss'),
+        this.destinationPath('generic/_layout.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/objects/_picturefill-bg.scss'),
-      this.destinationPath('objects/_picturefill-bg.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/generic/_smooth-scrolling.scss'),
+        this.destinationPath('generic/_smooth-scrolling.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/objects/_responsive-video.scss'),
-      this.destinationPath('objects/_responsive-video.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/objects/_layout.scss'),
+        this.destinationPath('objects/_layout.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/objects/_square.scss'),
-      this.destinationPath('objects/_square.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/objects/_picturefill-bg.scss'),
+        this.destinationPath('objects/_picturefill-bg.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/settings/_breakpoints.scss'),
-      this.destinationPath('settings/_breakpoints.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/objects/_responsive-video.scss'),
+        this.destinationPath('objects/_responsive-video.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/settings/_colours.scss'),
-      this.destinationPath('settings/_colours.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/objects/_square.scss'),
+        this.destinationPath('objects/_square.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/settings/_easing.scss'),
-      this.destinationPath('settings/_easing.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/settings/_breakpoints.scss'),
+        this.destinationPath('settings/_breakpoints.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/settings/_fonts.scss'),
-      this.destinationPath('settings/_fonts.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/settings/_colours.scss'),
+        this.destinationPath('settings/_colours.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/settings/_layout.scss'),
-      this.destinationPath('settings/_layout.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/settings/_easing.scss'),
+        this.destinationPath('settings/_easing.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/tools/_font-size.scss'),
-      this.destinationPath('tools/_font-size.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/settings/_fonts.scss'),
+        this.destinationPath('settings/_fonts.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/tools/_math.scss'),
-      this.destinationPath('tools/_math.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/settings/_layout.scss'),
+        this.destinationPath('settings/_layout.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/tools/_media-queries.scss'),
-      this.destinationPath('tools/_media-queries.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/tools/_font-size.scss'),
+        this.destinationPath('tools/_font-size.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/tools/_shapes.scss'),
-      this.destinationPath('tools/_shapes.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/tools/_math.scss'),
+        this.destinationPath('tools/_math.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/tools/_widths.scss'),
-      this.destinationPath('tools/_widths.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/tools/_media-queries.scss'),
+        this.destinationPath('tools/_media-queries.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/trumps/_clearfix.scss'),
-      this.destinationPath('trumps/_clearfix.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/tools/_shapes.scss'),
+        this.destinationPath('tools/_shapes.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/trumps/_visibility.scss'),
-      this.destinationPath('trumps/_visibility.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/tools/_widths.scss'),
+        this.destinationPath('tools/_widths.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/trumps/_widths-responsive.scss'),
-      this.destinationPath('trumps/_widths-responsive.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/trumps/_clearfix.scss'),
+        this.destinationPath('trumps/_clearfix.scss')
+      );
 
-    this.fs.copy(
-      this.templatePath('src/trumps/_widths.scss'),
-      this.destinationPath('trumps/_widths.scss')
-    );
+      this.fs.copy(
+        this.templatePath('src/trumps/_visibility.scss'),
+        this.destinationPath('trumps/_visibility.scss')
+      );
+
+      this.fs.copy(
+        this.templatePath('src/trumps/_widths-responsive.scss'),
+        this.destinationPath('trumps/_widths-responsive.scss')
+      );
+
+      this.fs.copy(
+        this.templatePath('src/trumps/_widths.scss'),
+        this.destinationPath('trumps/_widths.scss')
+      );
+    } else {
+      this.fs.write(this.destinationPath('base/.gitkeep'), '');
+      this.fs.write(this.destinationPath('generic/.gitkeep'), '');
+      this.fs.write(this.destinationPath('objects/.gitkeep'), '');
+      this.fs.write(this.destinationPath('settings/.gitkeep'), '');
+      this.fs.write(this.destinationPath('tools/.gitkeep'), '');
+      this.fs.write(this.destinationPath('trumps/.gitkeep'), '');
+    }
   }
 };

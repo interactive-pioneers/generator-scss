ca845b37ffb45382639f9df1fa75d55f7390d7d6
Georg Meyer <gm@interactive-pioneers.de>
2017-08-24 07:20:23 +0200

    Include normalize css as dev dependency [ci skip]

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,4 +13,7 @@ module.exports = class extends Generator {
       this.destinationPath('components/.gitkeep')
     );
   }
+  install() {
+    this.npmInstall(['normalize-scss'], {'save-dev': true});
+  }
 };
